a2120ab581d2e6b1e8346d4572473afb1308ebcd
shimWooJun <swj97@naver.com>
2023-01-26 17:58:02 +0900

    20230126

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex11/c/Exam0110.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex11/c/Exam0110.java
@@ -4,9 +4,16 @@ package com.eomcs.oop.ex11.c;
 class X {} // Top Level Class
 
 class A {
+  static void m1(int a) {
+    // static 메서드는 A의 인스턴스를 저장하는 this 라는 변수가 없다.
+  }
+
+  void m2(int b) {
+    // non-static 메서드는 A의 인스턴스를 저장할 this라는 변수가 있다.
+  }
 
   class X { // inner class
-    // 컴파일러는 inner 클래스를 컴파일 할 때 다음과 같이 
+    // 컴파일러는 inner 클래스를 컴파일 할 때 다음과 같이
     // - 바깥 클래스의 인스턴스 주소를 저장할 필드를 추가하고,
     // - 바깥 클래스의 인스턴스의 주소를 파라미터로 받는 생성자를 만든다.
     //
@@ -37,7 +44,7 @@ public class Exam0110 {
     //2) 바깥 클래스의 인스턴스 주소를 사용하여 inner class의 인스턴스 생성
     obj = outer.new X();
 
-    // 컴파일러는 컴파일 할 때 다음과 같이 
+    // 컴파일러는 컴파일 할 때 다음과 같이
     // - 바깥 클래스의 객체를 생성자에 전달하는 코드로 변경한다.
     //    obj = new A.X(outer);
 
---
+++ myapp/app/src/main/java/bitcamp/util/AbstractList.java
@@ -0,0 +1,46 @@
+package bitcamp.util;
+
+public abstract class AbstractList implements List {
+
+  protected int size;
+
+  @Override
+  public Object get(int index) {
+    if (index < 0 || index >= this.size) {
+      throw new IndexOutOfBoundsException("인덱스가 무효합니다.");
+    }
+    return null;
+  }
+
+  @Override
+  public int size() {
+    return this.size;
+  }
+
+
+  // iterator() 클래스에서만 사용하는 클래스라면
+  // 이 클래스 안에 두는 것이 유지보수에 좋다.
+  // - 인스턴스를 한 개만 만들어  사용하고, 클래스의 크기도 작다면
+  // 익명 클래스로 만드는 것이 코드를 간결하게 만든다.
+  // => anonymous class = 클래스 정의 + 객체 생성 코드
+
+  @Override
+  public Iterator iterator(){
+
+    // => return 문 + anonymous class
+    return new Iterator(){
+      int cursor;
+      @Override
+      public boolean hasNext() {
+        return cursor >= 0 && cursor < AbstractList.this.size();
+      }
+      @Override
+      public Object next() {
+        return AbstractList.this.get(cursor++);
+      }
+
+    };
+
+  }
+
+}
